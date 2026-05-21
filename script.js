434aa17d1da6d5ab37e8b015b61779ab9e92f867
Yujin-0224 <yooyoo5435@gmail.com>
2026-05-21 12:19:24 +0900

    Revert "Simplify top news preview cards"
    This reverts commit 63740c09e717d9466890ca1e42b01594b3017029.

--- script.js
+++ script.js
@@ -1512,8 +1512,9 @@ function renderTopNewsCard(article) {
     <article class="top-news-card" data-news-id="${escapeHtml(article.id || "")}" tabindex="0" role="button">
       <img src="${image}" alt="${title}" />
       <div>
+        <time datetime="${escapeHtml(article.datetime || "")}">${escapeHtml(article.date || article.datetime || "")}</time>
         <h3>${title}</h3>
-        <p>${lead}</p>
+        <p>${lead} <span>→</span></p>
       </div>
     </article>
   `;
